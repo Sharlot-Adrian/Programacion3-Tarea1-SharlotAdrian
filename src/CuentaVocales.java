13ae6e84ca21c7b1a50be0aacf33c0977015ece4
Sharlot <sharlot_adrian@ucne.edu.do>
2026-05-16 18:30:19 -0400

    Se agrego la clase cuentaVocales para el case #4 del menu

---
+++ src/CuentaVocales.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+public class CuentaVocales {
+
+    Scanner scanner = new Scanner(System.in);
+
+    private int contarVocales(String palabra){
+
+        palabra = palabra.toUpperCase();
+        char [] letras = palabra.toCharArray();
+        int contador = 0;
+
+        for (int i= 0; i < letras.length; i++ ){
+            
+            if (letras[i] == 'A' || letras[i] == 'E' || letras[i] == 'I' || letras[i] == 'O' || letras[i] == 'U' ){
+
+                contador++;
+            }
+
+        }
+
+        return contador;
+
+    }
+
+    public void mostarMenu(){
+
+        System.out.println("\n [CONTAR VOCALES]");
+        System.out.println("*----------------------------------*");
+        System.out.println("Ingrese la palabra cuyas vocales desea contar: ");
+        String palabra = scanner.nextLine();
+        System.out.println("*----------------------------------*");
+
+        System.out.println("La palabra tiene "+contarVocales(palabra) + "letras.");
+
+    }
+
+}
