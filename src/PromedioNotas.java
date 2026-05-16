0a731b3a44454525f6719199e78966c4b318ff60
Sharlot <sharlot_adrian@ucne.edu.do>
2026-05-16 18:51:50 -0400

    Se agrego la clase PromedioNotas, pensando en el case #5 del menu

---
+++ src/PromedioNotas.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+public class PromedioNotas {
+
+    Scanner scanner = new Scanner (System.in);
+
+
+    private double obtenerPromedio (int cantidadNotasPublicadas){
+
+        int sumatoria = 0;
+        int nota = 0;
+        double promedio = 0;
+    
+        for (int i = 0; i < cantidadNotasPublicadas; i++ ){
+
+            System.out.printf("Ingrese su nota #%d", i + 1);
+            nota = scanner.nextInt();
+            sumatoria += nota;
+
+        }
+
+        promedio = sumatoria / cantidadNotasPublicadas;
+        return promedio;
+
+    }
+
+    public void mostrarMenu(){
+
+        int cantidadNotasPublicadas = 0;
+        System.out.println("\n[ PROMEDIO DE NOTAS ]");
+        System.out.println("*----------------------------------*");
+        System.out.println("Ingrese el total de materias de las que ya tiene la nota: ");
+
+        cantidadNotasPublicadas = scanner.nextInt();
+        System.out.println("Sus promedio de notas es igual a: "+ obtenerPromedio(cantidadNotasPublicadas));
+        System.out.println("*----------------------------------*");
+
+    }
+}
