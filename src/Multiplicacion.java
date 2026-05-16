ea222d67af6d63e44b265bcb93a97bbf5222c69a
Sharlot <sharlot_adrian@ucne.edu.do>
2026-05-16 16:33:35 -0400

    Se creo la clase multiplicacion para utilizarse para la opcion #3 del menu, tablas de multiplicar

---
+++ src/Multiplicacion.java
@@ -0,0 +1,17 @@
+public class Multiplicacion {
+    
+    private int numero;
+
+    public Multiplicacion( int numero ){
+        this.numero = numero;
+    }
+
+    public void tablasHasta10(){
+
+        System.out.println("");
+
+        for (int i = 1; i <=10; i++){
+            System.out.printf("%d * %d = %d", i, this.numero, i * this.numero);
+        }
+    }
+}
