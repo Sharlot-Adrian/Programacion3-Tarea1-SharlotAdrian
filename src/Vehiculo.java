3242fbd5fc3246bd770e1f33f7850fdb7a9f5183
Sharlot <sharlot_adrian@ucne.edu.do>
2026-05-17 09:19:11 -0400

    Creacion de clase abstracta -Vehiculo-

---
+++ src/Vehiculo.java
@@ -0,0 +1,53 @@
+public abstract class Vehiculo {
+
+    private String modelo;
+    private String marca;
+    private int year;
+    private String color;
+
+
+    public Vehiculo(String modelo, String marca, int year, String color){
+        setModelo(modelo);
+        setMarca(marca);
+        setYear(year);
+        setColor(color);
+    }
+
+    public String getModelo(){
+        return modelo;
+    }
+
+    public String getMarca(){
+        return marca;
+    }
+
+    public int getYear(){
+        return year;
+    }
+
+    public String color(){
+        return color;
+    }
+
+    public void setModelo(String modelo){
+        this.modelo = modelo;
+    }
+
+    public void setMarca(String marca){
+        this.marca = marca;
+    }
+
+    public void setYear(int year){
+        this.year = year;
+    }
+
+    public void setColor(String color){
+        this.color = color;
+    }
+
+    public void acelerar(){
+        
+        System.out.println("El vehiculo esta en movimiento. ");
+    }
+
+}
